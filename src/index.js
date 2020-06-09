7c2e0acd6b4a65b05640c180485ae7459c7b92ff
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2020-06-09 14:56:38 +0300

    Init proj

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const telegram = require('telegram-bot-api')
-const config = require('config')
-
-const token = config.get('token')
-console.log(token)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import TelegramBot from 'node-telegram-bot-api'
+import config from 'config'
+
+const token: string = config.get('token')
+
