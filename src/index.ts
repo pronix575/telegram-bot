80795f5a656221f15628d25b9a77021f8e9c3fac
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2020-06-09 15:41:44 +0300

    Something

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ import TelegramBot from 'node-telegram-bot-api'
 import config from 'config'
 
 const token: string = config.get('token')
+
+const bot = new TelegramBot(token, { polling: true })
+
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, 'Received your message');
+})
